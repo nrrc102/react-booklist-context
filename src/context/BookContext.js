7d27d777c5d010cabca3aab5a2692788a9d8d1fb
nrrc102 <mumuree10@gmail.com>
2021-04-01 12:56:09 +0800

    reading list pt1

---
+++ src/context/BookContext.js
@@ -0,0 +1,29 @@
+import React,{createContext, useState} from 'react';
+import uuid from 'uuid/dist/v1';
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+    const [books, setBooks] = useState([
+        {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
+        {title: 'the final empire', author: 'brandon sanderson', id: 2} 
+    ]);
+
+    const addBook = (title, author) => {
+        setBooks([...books, {title, author, id: uuid()}]);
+        //or
+        // setBooks([...books, {title: title, author: author, id: uuid()}]);
+    };
+    
+    const removeBook = (id) => {
+        setBooks(books.filter(book => book.id !== id));
+    };
+    
+    return(
+        <BookContext.Provider value={{...books, addBook, removeBook}}>
+            {props.children}
+        </BookContext.Provider>
+    )
+}
+
+export default BookContextProvider;
